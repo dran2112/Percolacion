81f82304a7d270c427a7199fadca94f97b8aa0c1
dran2112 <dran_2112@hotmail.com>
2017-04-07 09:05:08 -0300

    Update de perco2
    *Solucioné el Segmentation fault.
    *Reetiqueta los clusters pero a veces la flashea

--- perco2.c
+++ perco2.c
@@ -15,7 +15,7 @@ void  actualizar(int *red, int *clase, int frag, int n);
 void  etiqueta_falsa(int *clase,int s1,int s2);
 void  corregir_etiqueta(int *red,int *clase,int n);
 void hoshen(int *red,int n);
-int   percola(int *red,int n);
+//int   percola(int *red,int n);
 
 
 int main(){
@@ -29,9 +29,10 @@ int main(){
    srand(time(NULL));
    llenar(red,n,prob);
 	red_ini = red;
-   hoshen(red,n);
 	imprimir(red_ini, n);
+   hoshen(red,n);
 	printf("\n \n");
+
 	imprimir(red, n);
 	
  /* for(i=0;i<z;i++)
@@ -63,7 +64,7 @@ int main(){
 void hoshen(int *red, int n){
 	int *clase;
 
-	clase = (int *)malloc(10*n*sizeof(int));
+	clase = (int *)malloc(30*n*sizeof(int));
 
 	clase[0] = 0; clase[1] = 1;
 
@@ -74,6 +75,33 @@ void hoshen(int *red, int n){
 void actualizar(int *red, int *clase, int frag, int n){  // actualiza cada nodo de la red según los nodos vecinos.
 	int i, j, nuevo, s;
 	nuevo = frag;
+
+	for(j=0;j<n;j++){
+				if(!red[0*n+j]) red[0*n+j]= red[0*n+j]; // CASO 1 no pasa nada
+				else if(red[0*n+j]){  // CASO 2 actualiza etiqueta.
+					red[0*n+j]=nuevo; 		
+					clase[nuevo] = nuevo;  // agrego una etiqueta al vector auxiliar "clase[]"
+					nuevo++;
+				} 
+				if(  red[0*n+j] && red[i*n+j-1]){  // 		      0
+					s = red[i*n+j-1];												// CASO 3 ---> X  1
+					red[i*n+j] = s;		//										Actualizao etiqueta
+				}
+	}
+
+	for(i=1;i<n;i++){
+				if(!red[i*n+0]) red[i*n+0]= red[i*n+0]; // CASO 1 no pasa nada
+				else if(red[i*n+0]){  // CASO 2 actualiza etiqueta.
+					red[i*n+0]=nuevo; 		
+					clase[nuevo] = nuevo;  // agrego una etiqueta al vector auxiliar "clase[]"
+					nuevo++;
+				} 
+				if(  red[i*n+0] && red[i*n+0-n]){  // 		      0
+					s = red[i*n+0-n];												// CASO 3 ---> X  1
+					red[i*n+0] = s;		//										Actualizao etiqueta
+				}
+	}
+
 	for (i=1; i<n; i++){
 		for (j=1; j<n; j++){
 				
@@ -95,6 +123,7 @@ void actualizar(int *red, int *clase, int frag, int n){  // actualiza cada nodo 
 		}
 	}
 	corregir_etiqueta(red, clase, n);
+//	corregir_etiqueta(red, clase, n);	
 }
 
 void etiqueta_falsa(int *clase, int s1, int s2){  // pone etiquetas falsas en vector auxiliar "clase"
@@ -104,23 +133,31 @@ void etiqueta_falsa(int *clase, int s1, int s2){  // pone etiquetas falsas en ve
 }
 
 void corregir_etiqueta(int *red, int *clase, int n){  // corrije etiquetas en la red.
-	int s1, s2, i, j;
-	for(i=0; i<n; i++){ 
-		for(j=0; j<n; j++){ 
+	int s0, s1, s2, i, j;
+	for(i=1; i<n; i++){ 
+		for(j=1; j<n; j++){ 
 
-			if( red[i*n+j] && red[i*n+j-n] && red[i*n+j-1] ){
-				s1 = red[i*n+j-n];  s2 = red[i*n+j-1];
+			if( red[i*n+j] && (red[i*n+j-n] || red[i*n+j-1]) ){
+				s1 = red[i*n+j-n];  s2 = red[i*n+j-1]; s0 = red[i*n+j];
 				while(clase[s1]<0)  s1 = -clase[s1];
 				while(clase[s2]<0)  s2 = -clase[s2];
 			
-				if(s1<s2){
+				if(s1<=s2 && s1>0){
 					clase[s2] = -s1;
 					red[i*n+j] = s1;
+					red[i*n+j-1] = s1;
+					red[i*n+j-n] = s1;
 				}
-				else if(s2<s1){
+				else if(s2<=s1 && s2>0){
 					clase[s1] = -s2;
 					red[i*n+j] = s2;
+					red[i*n+j-1] = s2;
+					red[i*n+j-n] = s2;
 				}		
+				else if(s0 < s1){
+					clase[s1] = -s0;
+					red[i*n+j-n] = s0;
+				} 
 			}
 		}
 	}
